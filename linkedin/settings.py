804112a2f797c28f048cff200b0630f2de2593bc
adityachoudhury29 <achoudhury2004@gmail.com>
2023-08-20 12:19:57 +0530

    db host change again

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'NAME': 'linkedin_clone',
         'USER': 'aditya',
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '172.19.0.3',
+        'HOST': 'db',
         'PORT': '5432',  # Default PostgreSQL port
     }
 }
