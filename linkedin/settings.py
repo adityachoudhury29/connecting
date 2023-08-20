1956ab4f24049bbd8c9c32aac28f1718c5b6f61a
adityachoudhury29 <achoudhury2004@gmail.com>
2023-08-20 15:29:24 +0530

    settings changes 3

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4c^r+q)bki2_%*6$u)gd0&hsoncglmh^-x9ph1we@%!9etkqr0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["connecting-kggc.onrender.com","192.168.30.73","192.168.203.73","localhost","127.0.0.1"]
 
@@ -151,3 +151,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+SESSION_COOKIE_SAMESITE = 'Lax'
+CSRF_COOKIE_SAMESITE = 'Lax'
